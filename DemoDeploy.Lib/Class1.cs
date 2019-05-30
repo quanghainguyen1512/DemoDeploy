2aa8262aef003ea2cab06dda995c98224719060c
Hai Nguyen <quanghai.ng1512@gmail.com>
2019-05-30 23:48:36 +0700

    Init

---
+++ DemoDeploy.Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DemoDeploy.Lib
+{
+    public static class Class1
+    {
+        public static string SomeFuckingString() {
+            return "This is that fucking string";
+        }
+    }
+}
